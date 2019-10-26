5dbbcd0e7cb2ea1b7daa17067c60c4eeb26984b9
Javier <julecia.ieu2017@student.ie.edu>
2019-10-26 14:52:54 +0200

    file reader setup

--- PairProgramming/src/com/company/Main.java
+++ PairProgramming/src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        LinkedList<String> alpha = new LinkedList<String>();
+
+        String word;
+        File unsorted = new File("unsorteddict.txt");
+        try {
+            Scanner sc = new Scanner(unsorted);
+            while (sc.hasNextLine()) {
+                word = sc.nextLine();
+            }
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
     }
 }
