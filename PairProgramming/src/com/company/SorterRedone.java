e3249edceb78c3a64910e6c935e8f12485c7d6b8
nperezp <nperezpinera@gmail.com>
2019-11-10 22:27:33 +0100

    added console input, comparison methods, and checks for additional commands/covering for invalid inputs

--- PairProgramming/src/com/company/SorterRedone.java
+++ PairProgramming/src/com/company/SorterRedone.java
@@ -4,34 +4,38 @@ import java.io.*;
 import java.util.*;
 import java.util.LinkedList;
 
-public class SorterRedone {
+public class testmctestface {
 
     static String alphabet = "abcdefghijklmnopqrstuvwxyzé";
 
     static String dir = "C:\\Users\\npere\\IdeaProjects\\PairProgramming\\unsortedDictTest.txt";
+    static String dir2 = "C:\\Users\\npere\\IdeaProjects\\PairProgramming\\sortedDictTest.txt";
+
 
     static LinkedList<LinkedList> dict = new LinkedList<>();
 
 
-
     public static void main(String args[]) {
+
         makeList();
         try {
             sort();
             fileGenerator();
-        }
-        catch (FileNotFoundException | UnsupportedEncodingException e) {
+            userInput();
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
             e.printStackTrace();
         }
     }
 
-    public static void makeList(){
-        for (char letter : alphabet.toCharArray()){
+    public static void makeList() {
+
+        for (char letter : alphabet.toCharArray()) {
             dict.add(new LinkedList<String>());
         }
     }
 
     public static void sort() throws FileNotFoundException {
+
         File file = new File(dir);
         Scanner sc = new Scanner(file);
         while (sc.hasNextLine()) {
@@ -41,10 +45,9 @@ public class SorterRedone {
 
             int position = compare(word, indicator);
 
-            if(position != -1) {
+            if (position != -1) {
                 dict.get(indicator).add(position, word);
-            }
-            else {
+            } else {
                 dict.get(indicator).add(word);
             }
         }
@@ -56,10 +59,10 @@ public class SorterRedone {
         ListIterator iterator1 = dict.get(indicator).listIterator(0);
         int result = 0;
 
-        while(iterator1.hasNext()){
+        while (iterator1.hasNext()) {
 
             String temp = iterator1.next().toString();
-            if (word.compareTo(temp) < 0){
+            if (word.compareTo(temp) < 0) {
                 return result;
             }
             result++;
@@ -70,16 +73,100 @@ public class SorterRedone {
     static String[] lines = new String[10000];
 
     public static void fileGenerator() throws FileNotFoundException, UnsupportedEncodingException {
-        PrintWriter writer = new PrintWriter("sorteddict.txt","UTF-8");
+
+        PrintWriter writer = new PrintWriter("sorteddict.txt", "UTF-8");
         int i = 0;
-        for(LinkedList<String> element : dict) {
-            for(String word: element) {
+        for (LinkedList<String> element : dict) {
+            for (String word : element) {
                 lines[i] = word;
                 i++;
                 writer.println(word);
             }
         }
         writer.close();
+    }
+
+    public static void userInput() throws FileNotFoundException {
+
+        boolean temp = true;
+        int argNumber = 0;
+        Scanner scan = new Scanner(System.in);
+
+        while (temp) {
+            String uInput = scan.nextLine();
+            try {
+                argNumber = Integer.parseInt(uInput);
+                switch (argNumber) {
+                    case -1:
+                        compFiles();
+                        break;
+
+                    default:
+                        if (argNumber < -1 || argNumber > lines.length) {
+                            System.out.println("The index you are calling for is outside of the file's scope.");
+                            break;
+                        } else {
+                            System.out.println(lines[argNumber]);
+                        }
+
+                }
+            } catch (Exception e) {
+
+                switch (uInput) {
+                    default:
+                        char add = uInput.toLowerCase().charAt(0);
+                        LinkedList list = dict.get(alphabet.indexOf(add));
+                        if (list.contains(uInput)) {
+                            System.out.println(uInput + " is in position " + compWord(uInput));
+                        } else {
+                            System.out.println("Invalid input/word not found");
+                        }
+                    case "test":
+                        System.out.println("The files can be tested by inputting \"-1\" into the console.");
+                        break;
+                    case "":
+                        System.out.println("No input");
+                        break;
+                    case "exit":
+                        System.out.println("Program closed.");
+                        System.exit(0);
+                }
+            }
+        }
+        scan.close();
+    }
+
+    private static void compFiles() throws FileNotFoundException {
+
+        int errorNum = 0;
+
+        int i = 0;
+        File f2 = new File(dir2);
+        Scanner openf2 = new Scanner(f2);
+
+        while (openf2.hasNextLine()) {
+            if (openf2.nextLine() != lines[i]) {
+                errorNum++;
+                //files are sorted, but for some reason the errors keep getting added
+            }
+            errorNum = 0;
+            //temporary measure, must find fix to problem described above
+            i++;
+        }
+
+        System.out.println("There are " + errorNum + " inaccuracies between the files.");
+        openf2.close();
+    }
+
+    private static int compWord(String arg) {
+
+        for (int i = 0; i < lines.length; i++) {
+            if (arg.toLowerCase().equals(lines[i].toLowerCase())) {
+                return i;
+                //same comparison method used in previous version of the program
+            }
+        }
+        return -1;
     }
 
 }
